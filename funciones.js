06cfdd5e129ddbcc3cfdf32168bf8d62607cf312
Lucas <lucasrossi90@gmail.com>
2016-05-24 15:45:41 -0300

    martincito|

--- funciones.js
+++ funciones.js
@@ -1,10 +1,13 @@
-function cargarDatos(){
-	numero = prompt('Ingrese el NÚMERO de factura');
-	fecha = prompt('Ingrese la FECHA de la factura');
-	cliente =  prompt('Ingrese el CLIENTE de la factura');
-	cuit = prompt('Ingrese el CUIT del cliente');
-	document.getElementById('nfact').innerHTML = numero;
-	document.getElementById('fecha').innerHTML = fecha;
-	document.getElementById('cliente').innerHTML = cliente;
-	document.getElementById('cuit').innerHTML = cuit;
+function imprimir(){
+		var valores = getElementsByTagName('input');
+		var largo = valores.length;
+				for (var i = 0, i <= largo, i++){
+					elem = valores[i];
+					cadaValor(elem);
+				}
+}
+
+function cadaValor(elem){
+		var valor = elem.value;
+		elem.parentElement.innerHTML = valor;
 }

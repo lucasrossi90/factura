0bc10db8e826083854fc0caaffb825c3bba9701c
Martin Rosso <mrosso10@gmail.com>
2016-05-25 01:41:10 -0300

    las cosas

--- funciones.js
+++ funciones.js
@@ -1,13 +1,42 @@
 function imprimir(){
-		var valores = document.getElementsByTagName("input");
-		var largo = valores.length;
-				for (var i = 0; i <= largo ;i++){
-					elem = valores[i];
-					cadaValor(elem);
-				}
+    var costos = document.getElementsByClassName("prod");
+    var subtot = 0;
+    var cant= 0;
+    var precio = 0;
+      for (var j = 0; j < costos.length; j++) {
+        cant = parseInt(costos[j].querySelectorAll('.cant')[0].value || 1);
+        precio = parseFloat(costos[j].querySelectorAll('.costo')[0].value || 0);
+        subtot = subtot + (cant * precio);
+      }
+
+      document.getElementById("subtotal").innerHTML = subtot;
+      document.getElementById("montoIVA").innerHTML= subtot * 0.21;
+      document.getElementById('total').innerHTML = subtot * 1.21;
+
+    var valores = document.getElementsByTagName("input");
+      for (var i = 0; i < valores.length ; i++){
+          elem = valores[i];
+          cadaValor(elem);
+        }
 }
 
 function cadaValor(elem){
 		var valor = elem.value;
-		elem.parentElement.innerHTML = valor;
+		elem.style = "display:none";
+    elem.parentElement.innerHTML += valor;
 }
+
+function cambiarIcono(select){
+    switch(select.value) {
+      case "B": document.getElementById("icono").innerHTML = "<span class='glyphicon glyphicon-bold'></span>";
+      break;
+
+      case "F": document.getElementById("icono").innerHTML = "<span class='glyphicon glyphicon-remove'></span>";
+      break;
+
+      case "A": document.getElementById("icono").innerHTML = "<span class='glyphicon glyphicon-font'></span>";
+      break;
+    }
+}
+
+document.getElementsByTagName('select')[0].onchange()
